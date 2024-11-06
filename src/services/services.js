daf77fa133baefec4d0c2173734c977fe00eb599
IanGWhite <97561180+IanGWhite@users.noreply.github.com>
2024-11-05 21:04:39 -0600

    fixed env and URL, login finished

--- src/services/services.js
+++ src/services/services.js
@@ -5,9 +5,9 @@ import Router from "../router.js";
 
 var baseurl = "";
 if (import.meta.env.DEV) {
-  baseurl = "http://localhost/tutorial/";
+  baseurl = "http://localhost:3025/resume-t5/"
 } else {
-  baseurl = "/tutorial/";
+  baseurl = "/resume-t5/";
 }
 
 const apiClient = axios.create({
