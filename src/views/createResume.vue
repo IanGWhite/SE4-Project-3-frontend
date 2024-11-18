adb675c15be173ec0df1e6fc05d3789c871478d3
ChargeChan <chandlerbilbo@gmail.com>
2024-11-18 10:58:24 -0600

    more progress on resume

--- src/views/createResume.vue
+++ src/views/createResume.vue
@@ -37,15 +37,78 @@ const generateResume1 = () => {
   var pageWidth = doc.internal.pageSize.getWidth(); 
   var pageHeight = doc.internal.pageSize.getHeight();
   var pageCenter = pageWidth/2
+  var currentY = 15;
 
-  doc.text('Hello world!', 10, 10)
-  doc.text(resumeName.value, 10, 10, {});
+  doc.setFontSize(20);
+  doc.text('First last', pageCenter, currentY, {align: "center"}); currentY += 5;
+  AddContactInfo(doc, currentY, pageCenter);
+  currentY += 15;
+
+  //summary header
+  doc.setFontSize(15);
+  doc.text("Professional Summary", 10, currentY, {});
+  currentY += 5;
+  doc.line(9, currentY, 200, currentY); currentY += 5;
+  //summary paragraph
   doc.setFontSize(12);
   var splitSummary = doc.splitTextToSize(professionalSummary.value, pageWidth-10);
-  doc.text(splitSummary, pageCenter, 40, {align: "center"})
+  doc.text(splitSummary, pageCenter, currentY, {align: "center"});
+  currentY += doc.getTextDimensions(splitSummary).h;
+  currentY += 10;
+  
+  //education header
+  doc.setFontSize(15);
+  doc.text("EDUCATION", 10, currentY, {});
+  currentY += 5;
+  doc.line(9, currentY, 200, currentY);
+  currentY += 5;
+  AddEducationInfo(doc, currentY);
+
+  
+  //doc.text("big ol test hahahahahaha", 10, currentY);
+  
   //doc.text(professionalSummary.value, pageWidth/2, 40, {align: "center"});
   doc.output('dataurlnewwindow');
 };
+
+const AddContactInfo = (doc, currentY, pageCenter) => {
+  doc.setFontSize(10);
+  var contactString = "";
+  contactString += "Oklahoma City | ";
+  contactString += "(555) 555-5555 | ";
+  contactString += "ike.theagle@oc.edu";
+  //if(sections.personalLink.checked.value)
+  if(sections.value.personalLink[0].checked)
+{
+  //contactString += personalLinks.type;
+  contactString += " | ";
+  contactString += personalLinks.link;
+}
+  doc.text(contactString, pageCenter, currentY, {align: "center"});
+}
+
+const AddEducationInfo = (doc, currentY) => {
+  var pageWidth = doc.internal.pageSize.getWidth(); 
+  var pageHeight = doc.internal.pageSize.getHeight();
+  var pageCenter = pageWidth/2
+  doc.setFontSize(10);
+  
+  for(let i=0; i<sections.value.education.length; i++)
+  {
+    
+    var edu = sections.value.education[i];
+    if(edu.checked)
+  {
+    // get school name and city and state
+    var tempConcat = "Oklahoma Christian University, Oklahoma City, OK"
+    doc.text(tempConcat, 10, currentY);
+    doc.text("Aug, 2021 - July 2025", pageWidth-10, currentY, {align:"right"});
+    currentY += 5;
+  }
+    
+
+  }
+}
 </script>
 
 <template>
